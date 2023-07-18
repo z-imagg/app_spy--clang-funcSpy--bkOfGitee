95c4d098d24a2ce937970f195585045ae5511075
prgrmz07 <prgrmz07@163.com>
2023-07-18 14:00:52 +0800

    CodeStyleChecker: ： c++实现类cpp文件拆开

--- include/CodeStyleChecker.h
+++ include/CodeStyleChecker.h
@@ -35,24 +35,7 @@ public:
                                        clang::SourceManager &SM)
       : Visitor(Context), SM(SM), MainTUOnly(MainFileOnly) {}
 
-  void HandleTranslationUnit(clang::ASTContext &Ctx) {
-    if (!MainTUOnly)
-      Visitor.TraverseDecl(Ctx.getTranslationUnitDecl());
-    else {
-      // Only visit declarations declared in the input TU
-      auto Decls = Ctx.getTranslationUnitDecl()->decls();
-      for (auto &Decl : Decls) {
-        // Ignore declarations out of the main translation unit.
-        //
-        // SourceManager::isInMainFile method takes into account locations
-        // expansion like macro expansion scenario and checks expansion
-        // location instead if spelling location if required.
-        if (!SM.isInMainFile(Decl->getLocation()))
-          continue;
-        Visitor.TraverseDecl(Decl);
-      }
-    }
-  }
+  void HandleTranslationUnit(clang::ASTContext &Ctx) ;
 
 private:
   CodeStyleCheckerVisitor Visitor;
--- lib/CodeStyleChecker/CodeStyleCheckerASTConsumer.cpp
+++ lib/CodeStyleChecker/CodeStyleCheckerASTConsumer.cpp
@@ -1,3 +1,20 @@
-//
-// Created by zz on 2023/7/18.
-//
+#include "CodeStyleChecker.h"
+
+void CodeStyleCheckerASTConsumer::HandleTranslationUnit(clang::ASTContext &Ctx) {
+  if (!MainTUOnly)
+    Visitor.TraverseDecl(Ctx.getTranslationUnitDecl());
+  else {
+    // Only visit declarations declared in the input TU
+    auto Decls = Ctx.getTranslationUnitDecl()->decls();
+    for (auto &Decl : Decls) {
+      // Ignore declarations out of the main translation unit.
+      //
+      // SourceManager::isInMainFile method takes into account locations
+      // expansion like macro expansion scenario and checks expansion
+      // location instead if spelling location if required.
+      if (!SM.isInMainFile(Decl->getLocation()))
+        continue;
+      Visitor.TraverseDecl(Decl);
+    }
+  }
+}
