b397ac36a407f8eb38595befe69d7ee91cf5d59c
prgrmz07 <prgrmz07@163.com>
2023-07-18 10:18:48 +0800

    CodeRefactor插件： PluginAction名字简化

--- tools/CodeRefactorMain.cpp
+++ tools/CodeRefactorMain.cpp
@@ -1,18 +1,3 @@
-//==============================================================================
-// FILE:
-//    CodeRefactorommenterMain.cpp
-//
-// DESCRIPTION:
-//    A standalone tool that runs the CodeRefactor plugin. See
-//    CodeRefactor.cpp for a complete description.
-//
-// USAGE:
-//    * ct-code-refactor --class-name=<class-in-which-to-rename> '\'
-//      --new-name=<new-method-name> --old-name=<old-method-name> input-file.cpp
-//
-//
-// License: The Unlicense
-//==============================================================================
 #include "CodeRefactor.h"
 
 #include "clang/Frontend/CompilerInstance.h"
@@ -24,41 +9,33 @@
 using namespace llvm;
 using namespace clang;
 
-//===----------------------------------------------------------------------===//
-// Command line options
-//===----------------------------------------------------------------------===//
 static cl::OptionCategory CodeRefactorCategory("ct-code-refactor options");
 
 
-//===----------------------------------------------------------------------===//
-// PluginASTAction
-//===----------------------------------------------------------------------===//
-class CodeRefactorPluginAction : public PluginASTAction {
+class CrPluginAction : public PluginASTAction {
 public:
-  explicit CodeRefactorPluginAction(){};
-  // Not used
+  explicit CrPluginAction(){};
   bool ParseArgs(const CompilerInstance &CI,
                  const std::vector<std::string> &args) override {
     return true;
   }
 
-  // Output the edit buffer for this translation unit
-  // void EndSourceFileAction() override {
-  //   RewriterForCodeRefactor
-  //       .getEditBuffer(RewriterForCodeRefactor.getSourceMgr().getMainFileID())
-  //       .write(llvm::outs());
-  // }
+   void EndSourceFileAction() override {
+//     rewriter
+//         .getEditBuffer(rewriter.getSourceMgr().getMainFileID())
+//         .write(llvm::outs()); //这里输出原始的?
+   }
 
   std::unique_ptr<ASTConsumer> CreateASTConsumer(CompilerInstance &CI,
                                                  StringRef file) override {//file:"/pubx/clang_plugin_demo/clang-tutor/test/CodeRefactor_Class.cpp"
-    RewriterForCodeRefactor.setSourceMgr(CI.getSourceManager(),
-                                         CI.getLangOpts());
+    rewriter.setSourceMgr(CI.getSourceManager(),
+                          CI.getLangOpts());
     return std::make_unique<CrASTConsumer>(
-        RewriterForCodeRefactor );
+            rewriter );
   }
 
 private:
-  Rewriter RewriterForCodeRefactor;
+  Rewriter rewriter;
 };
 
 //===----------------------------------------------------------------------===//
@@ -68,15 +45,15 @@ int main(int Argc, const char **Argv) {
   Expected<tooling::CommonOptionsParser> eOptParser =
       clang::tooling::CommonOptionsParser::create(Argc, Argv,
                                                   CodeRefactorCategory);
-  if (auto E = eOptParser.takeError()) {
+  if (auto error = eOptParser.takeError()) {
     errs() << "Problem constructing CommonOptionsParser "
-           << toString(std::move(E)) << '\n';
+           << toString(std::move(error)) << '\n';
     return EXIT_FAILURE;
   }
-  clang::tooling::RefactoringTool Tool(eOptParser->getCompilations(),
+  clang::tooling::RefactoringTool tool(eOptParser->getCompilations(),
                                        eOptParser->getSourcePathList());
 
-  return Tool.runAndSave(
-      clang::tooling::newFrontendActionFactory<CodeRefactorPluginAction>()
+  return tool.runAndSave(
+      clang::tooling::newFrontendActionFactory<CrPluginAction>()
           .get());
 }
