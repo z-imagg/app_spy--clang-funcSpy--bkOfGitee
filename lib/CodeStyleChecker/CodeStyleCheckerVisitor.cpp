b0f8b19060c0a93efeac2a2546e14a152ad8ac6d
prgrmz07 <prgrmz07@163.com>
2023-07-18 12:04:21 +0800

    CodeStyleChecker: ： c++实现类cpp文件拆开

--- lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
+++ lib/CodeStyleChecker/CodeStyleCheckerVisitor.cpp
@@ -19,8 +19,8 @@ bool CodeStyleCheckerVisitor::VisitCXXRecordDecl(CXXRecordDecl *Decl) {
 }
 
 bool CodeStyleCheckerVisitor::VisitFunctionDecl(FunctionDecl *Decl) {
-    //这里在遍历函数声明，参考： https://stackoverflow.com/questions/39529480/is-there-a-way-to-get-source-code-with-macro-expanded-using-clang-api
-    //写出 打印出宏展开后的样子
+  //这里在遍历函数声明，参考： https://stackoverflow.com/questions/39529480/is-there-a-way-to-get-source-code-with-macro-expanded-using-clang-api
+  //写出 打印出宏展开后的样子
 //    std::string s;
 //    llvm::raw_string_ostream sos(s);
 //    PrintingPolicy pp(compiler->getLangOpts());
@@ -28,7 +28,7 @@ bool CodeStyleCheckerVisitor::VisitFunctionDecl(FunctionDecl *Decl) {
 //    /*DeclPrinter*/ DP(sos, pp, 0 /* indent */, true /* PrintInstantiation */);
 //    DP.Visit(functionDecl);
 
-    // Skip user-defined conversion operators/functions:
+  // Skip user-defined conversion operators/functions:
   //    * https://en.cppreference.com/w/cpp/language/cast_operator
   if (isa<CXXConversionDecl>(Decl))
     return true;
@@ -72,15 +72,15 @@ void CodeStyleCheckerVisitor::checkNoUnderscoreInName(NamedDecl *Decl) {
   Hint.erase(end_pos, Hint.end());
 
   FixItHint FixItHint = FixItHint::CreateReplacement(
-      SourceRange(Decl->getLocation(),
-                  Decl->getLocation().getLocWithOffset(Name.size())),
-      Hint);
+          SourceRange(Decl->getLocation(),
+                      Decl->getLocation().getLocWithOffset(Name.size())),
+          Hint);
 
   DiagnosticsEngine &DiagEngine = Ctx->getDiagnostics();
   unsigned DiagID = DiagEngine.getCustomDiagID(DiagnosticsEngine::Warning,
                                                "`_` in names is not allowed");
   SourceLocation UnderscoreLoc =
-      Decl->getLocation().getLocWithOffset(underscorePos);
+          Decl->getLocation().getLocWithOffset(underscorePos);
   DiagEngine.Report(UnderscoreLoc, DiagID).AddFixItHint(FixItHint);
 }
 
@@ -96,15 +96,15 @@ void CodeStyleCheckerVisitor::checkNameStartsWithLowerCase(NamedDecl *Decl) {
   std::string Hint = Name;
   Hint[0] = toLowercase(FirstChar);
   FixItHint FixItHint = FixItHint::CreateReplacement(
-      SourceRange(Decl->getLocation(),
-                  Decl->getLocation().getLocWithOffset(Name.size())),
-      Hint);
+          SourceRange(Decl->getLocation(),
+                      Decl->getLocation().getLocWithOffset(Name.size())),
+          Hint);
 
   // Construct the diagnostic
   DiagnosticsEngine &DiagEngine = Ctx->getDiagnostics();
   unsigned DiagID = DiagEngine.getCustomDiagID(
-      DiagnosticsEngine::Warning,
-      "Function names should start with lower-case letter");
+          DiagnosticsEngine::Warning,
+          "Function names should start with lower-case letter");
   DiagEngine.Report(Decl->getLocation(), DiagID) << FixItHint;
 }
 
@@ -120,51 +120,14 @@ void CodeStyleCheckerVisitor::checkNameStartsWithUpperCase(NamedDecl *Decl) {
   std::string Hint = Name;
   Hint[0] = toUppercase(FirstChar);
   FixItHint FixItHint = FixItHint::CreateReplacement(
-      SourceRange(Decl->getLocation(),
-                  Decl->getLocation().getLocWithOffset(Name.size())),
-      Hint);
+          SourceRange(Decl->getLocation(),
+                      Decl->getLocation().getLocWithOffset(Name.size())),
+          Hint);
 
   // Construct the diagnostic
   DiagnosticsEngine &DiagEngine = Ctx->getDiagnostics();
   unsigned DiagID = DiagEngine.getCustomDiagID(
-      DiagnosticsEngine::Warning,
-      "Type and variable names should start with upper-case letter");
+          DiagnosticsEngine::Warning,
+          "Type and variable names should start with upper-case letter");
   DiagEngine.Report(Decl->getLocation(), DiagID) << FixItHint;
 }
-
-class CSCASTAction : public PluginASTAction {
-public:
-  std::unique_ptr<ASTConsumer>
-  CreateASTConsumer(CompilerInstance &Compiler,
-                    llvm::StringRef InFile) override {
-    return std::make_unique<CodeStyleCheckerASTConsumer>(
-        &Compiler.getASTContext(), MainTuOnly, Compiler.getSourceManager());
-  }
-
-  bool ParseArgs(const CompilerInstance &CI,
-                 const std::vector<std::string> &Args) override {
-    for (StringRef Arg : Args) {
-      if (Arg.startswith("-main-tu-only="))
-        MainTuOnly =
-            Arg.substr(strlen("-main-tu-only=")).equals_insensitive("true");
-      else if (Arg.startswith("-help"))
-        PrintHelp(llvm::errs());
-      else
-        return false;
-    }
-
-    return true;
-  }
-
-  void PrintHelp(llvm::raw_ostream &ros) {
-    ros << "Help for CodeStyleChecker plugin goes here\n";
-  }
-
-private:
-  bool MainTuOnly = true;
-};
-
-static clang::FrontendPluginRegistry::Add<CSCASTAction>
-    X(/*Name=*/"CSC",
-      /*Description=*/"Checks whether class, variable and function names "
-                      "adhere to LLVM's guidelines");
