7525c02d4376de8647df4f57061dff723926b002
prgrmz07 <prgrmz07@163.com>
2023-07-26 06:29:45 +0800

    运行结果/fallthrough下一行前不要插入

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,18 +1,25 @@
+#include "t_clock_tick.h"
 
 #define LLVM_FALLTHROUGH [[gnu::fallthrough]]
 
 char topOutFunc000(float f1, double d2){
-  char ch;
-  int val;
-  switch(val){
+  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+char ch;
+  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+int val;
+  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+switch(val){
     case 1:{
-      break;
+      X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+break;
     }
     case2:
       val++;
     case 3: {
-      ch+=val;
-    --ch;  
+      X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+ch+=val;
+    X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+--ch;  
     LLVM_FALLTHROUGH; //本fallthrough后面不能插入，注意本fallthrough此时并不在块尾
   case 4:
     
@@ -20,12 +27,17 @@ char topOutFunc000(float f1, double d2){
     if (val>9)
       break;
 
-    int x,  y, z=1;
-    y+=z;
-    break;
-    }
+    X__t_clock_tick(/*栈生*/3, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+int x,  y, z=1;
+    X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+y+=z;
+    X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+break;
+    X__t_clock_tick(/*栈生*/0, /*栈死*/3, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+}
     
   }
+X__t_clock_tick(/*栈生*/0, /*栈死*/2, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
 }
 
  
