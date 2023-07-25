517ec78e07db0b2dbd0e85a9b941b922d6edd978
prgrmz07 <prgrmz07@163.com>
2023-07-26 00:08:21 +0800

    运行结果/不在fallthrough语句后插入

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -1,14 +1,20 @@
+#include "t_clock_tick.h"
 #define LLVM_FALLTHROUGH [[gnu::fallthrough]]
 
 void f1(){
-  int s;
-  switch (s) {
+  X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+int s;
+  X__t_clock_tick(/*栈生*/0, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+switch (s) {
     case 1:
     {
-      int age;/*由于fallthrough语句后不能插入语句，否则语法错误。 按理释放语句应该插入在fallthrough之前，但该语句以宏出现，实测插入在LLVM_FALLTHROUGH之前无效。 导致释放语句被插入在倒数第二条语句之后。*/
+      X__t_clock_tick(/*栈生*/1, /*栈死*/0, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+int age;X__t_clock_tick(/*栈生*/0, /*栈死*/1, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
+/*由于fallthrough语句后不能插入语句，否则语法错误。 按理释放语句应该插入在fallthrough之前，但该语句以宏出现，实测插入在LLVM_FALLTHROUGH之前无效。 导致释放语句被插入在倒数第二条语句之后。*/
       LLVM_FALLTHROUGH;
     }
     case 2:
       break;
   }
+X__t_clock_tick(/*栈生*/0, /*栈死*/1, /*堆生*/0, /*堆死*/0);//TraverseCompoundStmt
 }
