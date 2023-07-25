180c89dbde6ffa06bd482c2c05bd09322a2a8250
prgrmz07 <prgrmz07@163.com>
2023-07-26 00:07:50 +0800

    注释

--- t_clock_tick/test_main.cpp
+++ t_clock_tick/test_main.cpp
@@ -5,7 +5,7 @@ void f1(){
   switch (s) {
     case 1:
     {
-      int age;
+      int age;/*由于fallthrough语句后不能插入语句，否则语法错误。 按理释放语句应该插入在fallthrough之前，但该语句以宏出现，实测插入在LLVM_FALLTHROUGH之前无效。 导致释放语句被插入在倒数第二条语句之后。*/
       LLVM_FALLTHROUGH;
     }
     case 2:
