df983719ac8a398b4f4a9efe4882d2904a92ebbb
prgrmz07 <prgrmz07@163.com>
2023-12-01 17:56:45 +0800

    build-libfmt.sh改造

--- build-libfmt.sh
+++ build-libfmt.sh
@@ -1,22 +1,86 @@
-cd /pubx/
+#!/bin/bash
 
-REPO_HOME=/pubx/fmt/
-BUILD_HOME=$REPO_HOME/build
+
+#加载 func.sh中的函数 ifelse
+source bash-simplify/func.sh
+source bash-simplify/dir_util.sh
+
+#当前脚本文件名, 此处 CurScriptF=example.sh
+CurScriptF=$(pwd)/$0
+
+
+PrjHmD="/crk"
+
+
+#0. 创建 PrjHome 目录 并 进入该目录
+
+
+ifelse  $CurScriptF $LINENO
+  true || test -e $PrjHome && test -d $PrjHome && \
+    true || "已有$PrjHome，无需创建" && \
+    true || cd $PrjHome && \
+  #else:
+    true || createDir_CurUsrOwn_EnterIt $PrjHome  && \
+      true || "目录$PrjHome 创建完成" && \
+
+
+
+#1. 下载git仓库 fmt.git
 
 #https://github.com/fmtlib/fmt.git
-git clone git@gitcode.net:pubx/fmtlib/fmt.git
-cd $REPO_HOME
-git checkout 10.0.0
+REPO_URL="https://prgrmz07:ChiPLVkNmksbkDe5pzMF@gitcode.net/pubx/fmtlib/fmt.git"
+RepoName="fmt"
+REPO_HOME="$PrjHmD/$RepoName"
+GitDir="$REPO_HOME/.git/"
+Ver="10.0.0"
+CmtId="a0b8a92e3d1532361c2f7feb63babc5c18d00ef2"
+ErrMsg1="错误, $Ver 的commitId不是 $CmtId, 退出码13"
 
-_CXX__FLAG=" -fno-omit-frame-pointer -Wall   -O0  -fPIC "
+function _gitClone_() {
+git clone   $REPO_URL $REPO_HOME && \
+git --git-dir=$GitDir checkout $Ver && \
+CurHeadCmtId=$(git --git-dir=$GitDir rev-parse HEAD) && \
+{ [ "X$CurHeadCmtId" == "X$CmtId" ]  || { echo $ErrMsg1 && exit 13 ;} ;} && \
+_=end
 
+}
 
-rm -fr $BUILD_HOME && mkdir $BUILD_HOME && cd $BUILD_HOME
+# set -x
+ifelse  $CurScriptF $LINENO
+  true || test -e $GitDir && test -d $GitDir && \
+    true || "已有fmt的git仓库,无需下载" && \
+    true || : && \
+  #else:
+    true || { pwd && _gitClone_  ;} && \
+      true || " $REPO_HOME git仓库 下载完成"
+
+# set +x
+#2. 编译 fmt
+BUILD_HOME="$REPO_HOME/build"
+LibFmtArchiv="$BUILD_HOME/libfmt.a"
+
+function _build_fmt() {
+
+_CXX__FLAG=" -fno-omit-frame-pointer -Wall   -O0  -fPIC " && \
+echo $BUILD_HOME && rm -fr $BUILD_HOME && mkdir $BUILD_HOME && cd $BUILD_HOME && \
 cmake \
 -DFMT_TEST=OFF \
 -DCMAKE_CXX_FLAGS="$_CXX__FLAG"  -DCMAKE_C_FLAGS="$_CXX__FLAG" \
--S $REPO_HOME
+-S $REPO_HOME && \
 
-make -j8
+make -j8 && \
 
-ls -lh $BUILD_HOME/libfmt.a
+echo "编译产物如下:" && \
+ls -lh $LibFmtArchiv && file $LibFmtArchiv && \
+
+_=end
+}
+
+ifelse  $CurScriptF $LINENO
+  true || test -f $LibFmtArchiv && \
+    true || "已编译出 $LibFmtArchiv,无需再次编译" && \
+    true || : && \
+  #else:
+    true || _build_fmt && \
+      true || "$RepoName 编译完成"
+########
