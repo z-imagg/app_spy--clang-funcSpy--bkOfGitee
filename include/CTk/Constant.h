0ae6a11b1dc3a02014edd5874e4f553c2d8bb3b6
prgrmz01@163.com <prgrmz01@163.com>
2023-12-12 23:13:36 +0800

    纠正提示消息

--- include/CTk/Constant.h
+++ include/CTk/Constant.h
@@ -11,7 +11,5 @@ public:
     //CollectIncMacro_PPCb::pragmaMsgFull决定了Constant::NameSpace_funcIdAsmIns的值的样式是 "命名空间:pragmaMessgae"
     const   std::string NameSpace_funcIdAsmIns = ":funcId_asm_inserted";
 
-    const   std::string funcIdBaseH = "funcIdBase.h";
-    const   std::string IncStmt_funcIdBaseH = "#include \"funcIdBase.h\"\n";
 };
 #endif //CTk_CONSTANT_H
