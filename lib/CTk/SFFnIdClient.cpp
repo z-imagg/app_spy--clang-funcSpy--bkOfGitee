fc8ff65715e86ad08911762ea8ce7c54e212edf3
prgrmz07 <prgrmz07@163.com>
2023-12-14 22:36:13 +0800

    草稿： http client

--- lib/CTk/SFFnIdClient.cpp
+++ lib/CTk/SFFnIdClient.cpp
@@ -1,6 +1,10 @@
 #include "CTk/SFFnIdClient.h"
 
 
+//#define CPPHTTPLIB_OPENSSL_SUPPORT
+#include "cpp_httplib/httplib.h"
+#include <string>
+
 void SFFnIdClient::genFuncAbsLocId(
 std::string srcFilePath,int funcDeclBeginPresumedLoc_line,int funcDeclBeginPresumedLoc_column
 ,
@@ -8,4 +12,22 @@ int &srcFileId_output, int &funcAbsLocId_output
         ){
     //TODO 用某 c++ web request lib 访问 本地 SFFnIdService, 解析结果, 并返回结果
     //SFFnId:Source File and Function Id
+
+
+// HTTP
+    httplib::Client client("localhost", 802);
+
+//    auto response = client.Get("/SrcFileFuncnIdGenService/genFuncAbsLocId");
+
+/**
+ * inline Result Client::Post(const std::string &path, const std::string &body,
+                           const std::string &content_type)
+ */
+    std::string url("/SrcFileFuncnIdGenService/genFuncAbsLocId");
+    std::string body("");//TODO srcFilePath,funcDeclBeginPresumedLoc_line,funcDeclBeginPresumedLoc_column
+    std::string content_type("application/json");
+    httplib::Result response = client.Post(url, body, content_type);
+    response->status;
+    std::cout << "响应"<<response->body << "\n";
+    //TODO 还是需要json解析
 }
