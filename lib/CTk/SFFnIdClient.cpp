337858b9b4239d84c7b36d05d2bf912e5f040b5a
prgrmz07 <prgrmz07@163.com>
2023-12-14 23:21:10 +0800

    草稿： http client（json）

--- lib/CTk/SFFnIdClient.cpp
+++ lib/CTk/SFFnIdClient.cpp
@@ -5,6 +5,10 @@
 #include "cpp_httplib/httplib.h"
 #include <string>
 
+
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
+
 void SFFnIdClient::genFuncAbsLocId(
 std::string srcFilePath,int funcDeclBeginPresumedLoc_line,int funcDeclBeginPresumedLoc_column
 ,
@@ -23,11 +27,12 @@ int &srcFileId_output, int &funcAbsLocId_output
  * inline Result Client::Post(const std::string &path, const std::string &body,
                            const std::string &content_type)
  */
-    std::string url("/SrcFileFuncnIdGenService/genFuncAbsLocId");
+    std::string url("/SrcFileFuncIdGenService/genFuncAbsLocId");
     std::string body("");//TODO srcFilePath,funcDeclBeginPresumedLoc_line,funcDeclBeginPresumedLoc_column
     std::string content_type("application/json");
     httplib::Result response = client.Post(url, body, content_type);
     response->status;
     std::cout << "响应"<<response->body << "\n";
+    nlohmann::basic_json respJson=json::parse(response->body);
     //TODO 还是需要json解析
 }
