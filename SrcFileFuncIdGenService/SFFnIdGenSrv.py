164bbb5345f6037e6f75013a629ead947ede5dd1
prgrmz07 <prgrmz07@163.com>
2023-12-14 23:46:47 +0800

    草稿 SrcFileFuncIdGenService：  tiangolo/fastapi.git   可调试

--- SrcFileFuncIdGenService/SFFnIdGenSrv.py
+++ SrcFileFuncIdGenService/SFFnIdGenSrv.py
@@ -10,8 +10,8 @@ https://github.com/tiangolo/fastapi.git
 
 
 
-不可调试 运行:
-uvicorn main:app --reload
+可调试 运行:
+python SFFnIdGenSrv.py
 
 swagger:
 http://127.0.0.1:8000/docs
@@ -19,9 +19,11 @@ http://127.0.0.1:8000/docs
 
 fastapi 可调试运行 需要改代码:
 https://cloud.tencent.com/developer/article/1878633
-
+https://fastapi.tiangolo.com/tutorial/debugging/?h=uvicorn.run#more-details
+https://www.uvicorn.org/
 
 """
+import uvicorn
 
 from typing import Union
 
@@ -38,3 +40,7 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
