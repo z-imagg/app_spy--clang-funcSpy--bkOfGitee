5eba86f446a87bf1dd97b234de78b2ed9de810ef
prgrmz01@163.com <prgrmz01@163.com>
2023-12-15 00:12:56 +0800

    草稿 SrcFileFuncIdGenService服务端能给出写死的响应了，理论上只差生成ID的逻辑了

--- SrcFileFuncIdGenService/SFFnIdGenSrv.py
+++ SrcFileFuncIdGenService/SFFnIdGenSrv.py
@@ -14,7 +14,7 @@ https://github.com/tiangolo/fastapi.git
 python SFFnIdGenSrv.py
 
 swagger:
-http://127.0.0.1:8000/docs
+http://127.0.0.1:8002/docs
 
 
 fastapi 可调试运行 需要改代码:
@@ -22,12 +22,35 @@ https://cloud.tencent.com/developer/article/1878633
 https://fastapi.tiangolo.com/tutorial/debugging/?h=uvicorn.run#more-details
 https://www.uvicorn.org/
 
+
+中文文档:
+https://fastapi.tiangolo.com/zh/tutorial/body-nested-models/
 """
 import uvicorn
 
 from typing import Union
 
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+#{请求
+class FuncDeclBeginPresumedLoc(BaseModel):
+    line: int
+    column: int
+
+class GenSFFnIdReq(BaseModel):
+    srcFilePath: str
+    funcDeclBeginPresumedLoc: FuncDeclBeginPresumedLoc
+#请求}
+
+#{响应
+
+class SFFnIdResp(BaseModel):
+    srcFileId:int
+    funcAbsLocId:int
+
+#响应}
+
 
 app = FastAPI()
 
@@ -36,10 +59,11 @@ app = FastAPI()
 def read_root():
     return {"access": "false"}
 
-
-@app.get("/SrcFileFuncIdGenService/genFuncAbsLocId")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/SrcFileFuncIdGenService/genFuncAbsLocId",response_model=SFFnIdResp)
+def read_item(genSFFnIdReq: GenSFFnIdReq):
+    #TODO : 根据输入请求 genSFFnIdReq , 生成输出响应 SFFnIdResp
+    respDto=SFFnIdResp(srcFileId=2000,funcAbsLocId=4)
+    return respDto
 
 
 if __name__ == "__main__":
