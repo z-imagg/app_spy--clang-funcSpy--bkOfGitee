90e4fd92b53c0a096f7fa46b8f80e5a406d23488
prgrmz01@163.com <prgrmz01@163.com>
2023-12-16 15:12:44 +0800

    服务端 无锁草稿完毕？调试结果正常，代码较乱

--- SrcFileFuncIdGenService/test.py
+++ SrcFileFuncIdGenService/test.py
@@ -4,7 +4,14 @@
 from Srv import SFFnIdResp, SFFnIdReq, genFuncAbsLocId, FnDclBgPrsmLoc
 from Srv import manager
 
-fnDclBgPrsmLoc:FnDclBgPrsmLoc=FnDclBgPrsmLoc(line=12,column=4)
-reqDto: SFFnIdReq=SFFnIdReq(srcFilePath="/tmp/xxx.c",fnDclBgPrsmLoc=fnDclBgPrsmLoc)
-respDto=genFuncAbsLocId(reqDto)
+reqDto1: SFFnIdReq=SFFnIdReq(srcFilePath="/tmp/xxx.c",fnDclBgPrsmLoc=FnDclBgPrsmLoc(line=12,column=4))
+reqDto2: SFFnIdReq=SFFnIdReq(srcFilePath="/tmp/xxx.c",fnDclBgPrsmLoc=FnDclBgPrsmLoc(line=99,column=22))
+reqDto3: SFFnIdReq=SFFnIdReq(srcFilePath="/tmp/user.c",fnDclBgPrsmLoc=FnDclBgPrsmLoc(line=99,column=22))
+reqDto4: SFFnIdReq=SFFnIdReq(srcFilePath="/tmp/user.c",fnDclBgPrsmLoc=FnDclBgPrsmLoc(line=342,column=111))
+reqDto5: SFFnIdReq=SFFnIdReq(srcFilePath="/tmp/user.c",fnDclBgPrsmLoc=FnDclBgPrsmLoc(line=555,column=3))
+respDto1=genFuncAbsLocId(reqDto1)
+respDto2=genFuncAbsLocId(reqDto2)
+respDto3=genFuncAbsLocId(reqDto3)
+respDto4=genFuncAbsLocId(reqDto4)
+respDto5=genFuncAbsLocId(reqDto5)
 end=True
