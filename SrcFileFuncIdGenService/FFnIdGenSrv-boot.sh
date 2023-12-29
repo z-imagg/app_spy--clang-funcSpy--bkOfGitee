31cb52b103723902136efa96539b9cd6e392e47e
prgrmz07 <prgrmz07@163.com>
2023-12-29 13:38:51 +0800

    makLnk

--- SrcFileFuncIdGenService/FFnIdGenSrv-boot.sh
+++ SrcFileFuncIdGenService/FFnIdGenSrv-boot.sh
@@ -30,10 +30,7 @@ pip  install fastapi && \
 pip  install "uvicorn[standard]" && \
 pip  install peewee && \
 
-lnkDest="/bal/clang-add-funcIdAsm/SrcFileFuncIdGenService/py_util" && \
-lnkSrc="/bal/py_util" && \
-{ [ -e $lnkDest ] || \
-ln -s $lnkSrc $$lnkDest ;} && \
+makLnk "/bal/py_util" "/bal/clang-add-funcIdAsm/SrcFileFuncIdGenService/py_util"
 
 #启动函数id生成服务
 { nohup python WebMain.py & } && \
