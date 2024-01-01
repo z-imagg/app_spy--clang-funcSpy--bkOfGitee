c90b3bc859502b9fc8385b4e192affeced9980ff
prgrmz07 <prgrmz07@163.com>
2024-01-01 22:11:13 +0800

    /bal/clang-add-funcIdAsm/SrcFileFuncIdGenService/fn.db

--- SrcFileFuncIdGenService/SqliteDB_FnIdDef.py
+++ SrcFileFuncIdGenService/SqliteDB_FnIdDef.py
@@ -33,7 +33,7 @@ class Func(SqliteBaseEntity):
 
 def fn_initDb(removeDbFile:bool=True):
 
-    fnDb:SqliteDatabase=initSqliteDb('fn.db',removeDbFile=removeDbFile)
+    fnDb:SqliteDatabase=initSqliteDb('/bal/clang-add-funcIdAsm/SrcFileFuncIdGenService/fn.db',removeDbFile=removeDbFile)
 
     SrcFile._meta.database = fnDb
     Func._meta.database = fnDb
