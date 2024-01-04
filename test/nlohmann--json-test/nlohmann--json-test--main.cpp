c6e486985f9e99463993fcf393c1ecbbe3e92fa4
prgrmz07 <prgrmz07@163.com>
2024-01-04 17:31:18 +0800

    取当前函数地址 例子，、编译  、运行正常

--- test/nlohmann--json-test/nlohmann--json-test--main.cpp
+++ test/nlohmann--json-test/nlohmann--json-test--main.cpp
@@ -1,13 +1,24 @@
 #include "CTk/SFFnIdClient.h"
 
 #include <iostream>
+/* 参考gcc内敛汇编:  https://www.cnblogs.com/sky-heaven/p/7561625.html */
+double func01(char ch, float flt){
+    int integer=ch*flt;
+    __asm__  __volatile__ (
+            "jmp 0f \n\t"
+            "or $0xFFFFFFFF,%%edi \n\t"
+            "or $11,%%edi \n\t"
+            "or %0,%%edi \n\t"
+            "0: \n\t"
+            :
+            : "m"(func01)
+            );
+    double  result=integer/ch+flt;
+    return result;
+}
 int main(int Argc, const char **Argv) {
+    func01('A', 999.0);
 
-    int srcFileId,funcAbsLocId;
-    SFFnIdClient::genFuncAbsLocId("userName.c",12,0,"calcAgeFunc",srcFileId,funcAbsLocId);
-
-    std::cout << "srcFileId:"<< srcFileId << "\n";
-    std::cout << "funcAbsLocId:"<< funcAbsLocId << "\n";
     return 0;
 }
 
