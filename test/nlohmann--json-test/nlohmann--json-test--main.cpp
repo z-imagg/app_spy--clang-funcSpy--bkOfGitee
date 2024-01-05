a995df1f402506730d820227e114885c7c10d658
prgrmz07 <prgrmz07@163.com>
2024-01-05 19:36:18 +0800

    gcc内敛汇编orL ， 函数地址只是相对地址， 没有演示出inline效果

--- test/nlohmann--json-test/nlohmann--json-test--main.cpp
+++ test/nlohmann--json-test/nlohmann--json-test--main.cpp
@@ -2,22 +2,24 @@
 
 #include <iostream>
 /* 参考gcc内敛汇编:  https://www.cnblogs.com/sky-heaven/p/7561625.html */
-inline double func01(char ch, float flt){
-    int integer=ch*flt;
+#define FUNC_ID 16
+static inline double func01(){
     __asm__  __volatile__ (
             "jmp 0f \n\t"
             "or $0xFFFFFFFF,%%edi \n\t"
-            "orL $16,%%edi \n\t"
             "orL %0,%%edi \n\t"
+            "orL %1,%%edi \n\t"
             "0: \n\t"
             :
-            : "m"(func01)
+            : "i"(1024+FUNC_ID),"m"(func01)
             );
-    double  result=integer/ch+flt;
-    return result;
+
+
+
+    return 0.5;
 }
 int main(int Argc, const char **Argv) {
-    func01('A', 999.0);
+    func01();
 
     return 0;
 }
