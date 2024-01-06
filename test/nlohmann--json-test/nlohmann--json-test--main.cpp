2d1e7e0003e4c3ea7ef7ca83ba7d28964c493bb9
prgrmz07 <prgrmz07@163.com>
2024-01-06 16:35:32 +0800

    rizinorg/cutter反汇编工具 再次表明orL指令的操作数大于256时占据4字节 小于256时占据1字节

--- test/nlohmann--json-test/nlohmann--json-test--main.cpp
+++ test/nlohmann--json-test/nlohmann--json-test--main.cpp
@@ -2,13 +2,12 @@
 
 #include <iostream>
 /* 参考gcc内敛汇编:  https://www.cnblogs.com/sky-heaven/p/7561625.html */
-#define FUNC_ID 16
+#define FUNC_ID 257
 static inline double func01(){
     __asm__  __volatile__ (
             "jmp 0f \n\t"               //0f 表示 0 forward 即 向前跳转到标号0
             "or $0xFFFFFFFF,%%edi \n\t" //$0xFFFFFFFF 表示 常数 0xFFFFFFFF
             "orL %0,%%edi \n\t"         //%0 指代 FUNC_ID 表示 函数id
-            "orL $257,%%edi \n\t"       //$257 是 常数257 表示 函数id
             "orL %1,%%edi \n\t"         //%0 指代 func01 表示 函数名
             "0: \n\t"                   //0: 表示 标号0
             :                           //输出 ，无输出
