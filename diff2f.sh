fcb3256abb391995d957891ed2af7046c4b10263
prgrmz07 <prgrmz07@163.com>
2024-01-07 19:35:58 +0800

    cmake 比较两次记录的所有变量差异save_vars__cur_diff_prev 草稿中（更优化）

--- diff2f.sh
+++ diff2f.sh
@@ -1,10 +1,18 @@
 #!/usr/bin/env bash
 
-#若参数少于2个，则退出（退出码为23）
-[ $# -lt 2 ] && exit 4
+# me.sh  ${_OutFPathPrefix} ${_prevVarsFIdx} ${gVarsFIdx}
+#若参数少于3个，则退出（退出码为55）
+[ $# -lt 3 ] && exit 55
 
-#echo -n "比较命令：‘diff $1 $2’ , "
+_OutFPathPrefix=$1
+_prevVarsFIdx=$2
+gVarsFIdx=$3
+_prevOutFPath="${_OutFPathPrefix}${_prevVarsFIdx}"
+outFPath="${_OutFPathPrefix}${gVarsFIdx}"
+resultF="${_OutFPathPrefix}diff_${_prevVarsFIdx}_${gVarsFIdx}"
+
+echo -n "比较命令：‘diff ${_prevOutFPath} ${outFPath}’ , "
 echo -n "差异行数："
-resultF=/bal/clang-add-funcIdAsm/diffText
-diff --side-by-side --suppress-common-lines $1 $2 > $resultF ;
-wc -l $resultF
+diff --side-by-side --suppress-common-lines ${_prevOutFPath} ${outFPath} > $resultF ;
+wc -l $resultF |  perl -pe 'chomp if eof'
+#用命令 “ perl -pe 'chomp if eof'  ” 删除wc输出的换行符号
--- vars_diff.cmake
+++ vars_diff.cmake
@@ -1,51 +1,56 @@
-set(Text_gVarsFIdxDesc "变量描述: 'cmake CACHE (INTERNAL)' 即 cmake全局(同级)变量")
+set(_Text_gVarsFIdxDesc "变量描述: 'cmake CACHE (INTERNAL)' 即 cmake全局(同级)变量")
+set(_OutFPathPrefix "/tmp/cmake_vars_")
 
+#如果从没有定义过全局变量gVarsFIdx 则定义该全局变量
 if(NOT DEFINED gVarsFIdx)
-    set(gVarsFIdx 0 CACHE INTERNAL  ${Text_gVarsFIdxDesc})
+    set(gVarsFIdx 0 CACHE INTERNAL  ${_Text_gVarsFIdxDesc})
     # 'CACHE INTERNAL'变量gVarsFIdx 写入在文件  ./cmake-build-debug/CMakeCache.txt :
-    #gVarsFIdx:INTERNAL=28
+    #gVarsFIdx:INTERNAL=数值
+
     message(STATUS "初始化 gVarsFIdx=${gVarsFIdx}")
 endif()
 
-function(save_cmake_vars)
+function(save_cmake_vars__cur_diff_prev)
 #    message(STATUS "函数进入 gVarsFIdx=${gVarsFIdx}")
-    MATH(EXPR prevVarsFIdx "${gVarsFIdx} - 1")
-    set(prevOutFPath "/tmp/cmake_vars_${prevVarsFIdx}.txt")
+    MATH(EXPR _prevVarsFIdx "${gVarsFIdx} - 1")
+#    set(_prevOutFPath "${_OutFPathPrefix}${_prevVarsFIdx}")
+
+    set(_curVarsFIdx "${gVarsFIdx}")
 
     # 获取 cmake变量们
     get_cmake_property(_varNames VARIABLES)
 
     # cmake变量们写入文件
-    set(outFPath "/tmp/cmake_vars_${gVarsFIdx}.txt")
-    file(WRITE ${outFPath} "")
+    set(_curOutFPath "${_OutFPathPrefix}${gVarsFIdx}")
+    file(WRITE ${_curOutFPath} "")
     foreach(_varName ${_varNames})
-        file(APPEND ${outFPath} "${_varName}=${${_varName}}\n")
+        if (_varName MATCHES "^_"  #忽略_开头的变量名
+                OR _varName STREQUAL "gVarsFIdx") #忽略此脚本用到的变量名gVarsFIdx
+#            message(STATUS "忽略变量名【${_varName}】")
+            continue()
+        endif()
+
+        #向文件中写入一行
+        file(APPEND ${_curOutFPath} "${_varName}=${${_varName}}\n")
     endforeach()
 
     #文件名下标 自增1
     MATH(EXPR _nextVarsFIdx "${gVarsFIdx} + 1")
-    set(gVarsFIdx ${_nextVarsFIdx} CACHE INTERNAL  ${Text_gVarsFIdxDesc})
+    set(gVarsFIdx ${_nextVarsFIdx} CACHE INTERNAL  ${_Text_gVarsFIdxDesc})
 #####
 # 执行 diff 命令
 execute_process(
-#        COMMAND diff ${outFPath} ${nextOutFPath}
-#        COMMAND   bash -x /bal/clang-add-funcIdAsm/diff2f.sh ${prevOutFPath} ${outFPath}
-        COMMAND   bash  /bal/clang-add-funcIdAsm/diff2f.sh ${prevOutFPath} ${outFPath}
+#        COMMAND   bash -x  ...
+        COMMAND   bash  /bal/clang-add-funcIdAsm/diff2f.sh ${_OutFPathPrefix} ${_prevVarsFIdx} ${_curVarsFIdx}
         RESULT_VARIABLE shExitCode
         OUTPUT_VARIABLE shStdOut
         ERROR_VARIABLE shStdErr
 )
 
-# 显示输出
-#if(shExitCode EQUAL 0)
-#    message(STATUS "文件相同")
-#else()
-    message(STATUS "退出码：【${shExitCode}】， 标准输出: 【${shStdOut}】， 错误输出:【${shStdErr}】")
-#endif()
-
-message(STATUS "函数出来 gVarsFIdx=${gVarsFIdx}")
+message(STATUS "保存cmake当前变量们并与上次变量们比较，退出码：【${shExitCode}】， 标准输出: 【${shStdOut}】， 错误输出:【${shStdErr}】")
+#message(STATUS "函数出来 gVarsFIdx=${gVarsFIdx}")
 
 endfunction()
 
 # 调用函数保存所有变量
-#save_cmake_vars()
+#save_cmake_vars__cur_diff_prev()
