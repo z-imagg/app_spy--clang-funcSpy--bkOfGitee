91559f8fc1b56f1745e1356e9071d53e75fe7c34
prgrmz07 <prgrmz07@163.com>
2024-01-08 11:39:08 +0800

    从llvm源码获知 --gcc-toolchain
    #注意看这里
    #到llvm-project源码中分支【release/15.x】搜索"Selected GCC installation"大概获知 --gcc-toolchain=/usr/lib/gcc/x86_64-linux-gnu/11 可以指定"Selected GCC installation"
    #llvm-project源码中分支【release/15.x】 ：  https://gitcode.net/pubz/llvm-project/-/tree/release/15.x

--- test/test_clang++/Build.sh
+++ test/test_clang++/Build.sh
@@ -12,7 +12,9 @@ InstalledDir: /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4
 Found candidate GCC installation: /usr/lib/gcc-cross/i686-linux-gnu/11
 Found candidate GCC installation: /usr/lib/gcc/x86_64-linux-gnu/11
 Found candidate GCC installation: /usr/lib/gcc/x86_64-linux-gnu/12
-Selected GCC installation: /usr/lib/gcc/x86_64-linux-gnu/12
+Selected GCC installation: /usr/lib/gcc/x86_64-linux-gnu/12  #注意看这里
+#到llvm-project源码中分支【release/15.x】搜索"Selected GCC installation"大概获知 --gcc-toolchain=/usr/lib/gcc/x86_64-linux-gnu/11 可以指定"Selected GCC installation"
+#llvm-project源码中分支【release/15.x】 ：  https://gitcode.net/pubz/llvm-project/-/tree/release/15.x
 Candidate multilib: .;@m64
 Selected multilib: .;@m64
  "/app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/bin/clang-15" -cc1 -triple x86_64-unknown-linux-gnu -emit-obj -mrelax-all --mrelax-relocations -disable-free -clear-ast-before-backend -disable-llvm-verifier -discard-value-names -main-file-name cpp_main_demo.cpp -mrelocation-model pic -pic-level 2 -pic-is-pie -mframe-pointer=all -fmath-errno -ffp-contract=on -fno-rounding-math -mconstructor-aliases -funwind-tables=2 -target-cpu x86-64 -tune-cpu generic -mllvm -treat-scalable-fixed-error-as-warning -debugger-tuning=gdb -v -fcoverage-compilation-dir=/bal/clang-add-funcIdAsm/test/test_clang++ -resource-dir /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0 -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/12/../../../../include/c++ -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/12/../../../../include/c++/x86_64-linux-gnu -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/12/../../../../include/c++/backward -internal-isystem /app/llvm_release_home/clang+llvm-15.0.0-x86_64-linux-gnu-rhel-8.4/lib/clang/15.0.0/include -internal-isystem /usr/local/include -internal-isystem /usr/lib/gcc/x86_64-linux-gnu/12/../../../../x86_64-linux-gnu/include -internal-externc-isystem /usr/include/x86_64-linux-gnu -internal-externc-isystem /include -internal-externc-isystem /usr/include -fdeprecated-macro -fdebug-compilation-dir=/bal/clang-add-funcIdAsm/test/test_clang++ -ferror-limit 19 -fgnuc-version=4.2.1 -fcxx-exceptions -fexceptions -fcolor-diagnostics -faddrsig -D__GCC_HAVE_DWARF2_CFI_ASM=1 -o /tmp/cpp_main_demo-c4eb74.o -x c++ cpp_main_demo.cpp
@@ -30,6 +32,7 @@ ignoring nonexistent directory "/include"
  /usr/include
 End of search list.
 '''
+
 
 #报错如下
 '''
