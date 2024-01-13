71ffdb86d25590144669ba2f7d96982698c5c89d
prgrmz07 <prgrmz07@163.com>
2024-01-13 17:42:44 +0800

    fn --> CapturedFn

--- SrcFileFuncIdGenService/SqliteDB_FnIdDef.py
+++ SrcFileFuncIdGenService/SqliteDB_FnIdDef.py
@@ -4,30 +4,30 @@
 from peewee import *
 import peewee
 
-from py_util.Util import IAmNotMain, initSqliteDb, closeSqliteDb, SqliteBaseEntity
-
+from py_util.Util import IAmNotMain, initSqliteDb, closeSqliteDb, SqliteBaseEntity, tabNameGen
 
 # fnDb = SqliteDatabase('fn.db')
 
 
 
-fn_db_name:str="fn"
+CapturedFn_dbName:str= "CapturedFn"
 def make_table_name__fn(model_class:peewee.ModelBase):
-    tbl_name:str=f't__{fn_db_name}__{model_class.__name__}'
-    print(f"fn表名:{tbl_name}")
+    entityClassName:str=model_class.__name__
+    simpleEntityClassName,tbl_name=tabNameGen(CapturedFn_dbName, entityClassName)
+    print(f"CapturedFn表名:{tbl_name}; 数据库简名:【{CapturedFn_dbName}】；实体类名：【{entityClassName}】; 简化实体类名：【{simpleEntityClassName}】")
     return   tbl_name
 
-class SqliteBaseEntity_Fn(SqliteBaseEntity):
+class SqliteBaseEntity_CapturedFn(SqliteBaseEntity):
     class Meta:
         table_function = make_table_name__fn
 
 
-class SrcFile(SqliteBaseEntity_Fn):
+class CapturedFn_SrcFile(SqliteBaseEntity_CapturedFn):
     fId = IntegerField(primary_key=True, unique=True, constraints=[SQL('AUTOINCREMENT')])
     sF= CharField()
     
 
-class Func(SqliteBaseEntity_Fn):
+class CapturedFn_Func(SqliteBaseEntity_CapturedFn):
     #sqlite的列类型是动态类型（列类型由插入的值类型决定），参考: https://www.cnblogs.com/qiupiaohujie/p/11981732.html
     fnAbsLctId = IntegerField(primary_key=True, unique=True, constraints=[SQL('AUTOINCREMENT')])
     fId = IntegerField()#引用SrcFile.fId
@@ -47,18 +47,18 @@ def fn_initDb(moveDbFile:bool=True):
 
     fnDb:SqliteDatabase=initSqliteDb('/bal/clang-add-funcIdAsm/SrcFileFuncIdGenService/fn.db',moveDbFile=moveDbFile)
 
-    SrcFile._meta.database = fnDb
-    Func._meta.database = fnDb
+    CapturedFn_SrcFile._meta.database = fnDb
+    CapturedFn_Func._meta.database = fnDb
 
     fnDb.connect()
-    fnDb.create_tables([SrcFile, Func])
+    fnDb.create_tables([CapturedFn_SrcFile, CapturedFn_Func])
 
     #用给定的值填充自增列并插入，以迫使自增列初始值为给定数值
-    emptyFunc: Func = Func.create(fnAbsLctId=FuncIdBegin,fId=-1,funcQualifiedName="",line=-1,column=-1)
-    Func.delete_by_id(FuncIdBegin)
+    emptyFunc: CapturedFn_Func = CapturedFn_Func.create(fnAbsLctId=FuncIdBegin, fId=-1, funcQualifiedName="", line=-1, column=-1)
+    CapturedFn_Func.delete_by_id(FuncIdBegin)
 
 def fn_closeDb():
-    closeSqliteDb(SrcFile._meta.database)
+    closeSqliteDb(CapturedFn_SrcFile._meta.database)
 
 
 
